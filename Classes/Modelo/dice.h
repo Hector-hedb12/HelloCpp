33053b59133e3c7773d9d8629a6a3232d2b176c6
jose <chinoxyz@gmail.com>
2014-06-06 11:56:21 +0200

    Now the dice distribution is uniform between 0..5. For that you have to use c++11 features. Remember adding this variable: "__GXX_EXPERIMENTAL_CXX0X__" with no value.

--- Classes/Modelo/dice.h
+++ Classes/Modelo/dice.h
@@ -5,17 +5,28 @@
  * Class Dice:
  * Pseudo Random 0 - 5
  */
+
+#include <random>
+#include <unordered_map>
+
+
+using namespace std;
+
 class dice{
 int x;
+default_random_engine generator;
+uniform_int_distribution<int> d1;
 public:
   dice(int s = 0){
-    x = rand()%6;
+	  generator = default_random_engine(rand());
+	  d1 = uniform_int_distribution<int>(0,5);
+	  next();
   }
   int recent(){
     return x;
   }
   int next(){
-    return x = rand()%6;
+    return x = d1(generator);
   }  
 };
 
--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -2,6 +2,15 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+#NDK_TOOLCHAIN_VERSION := 4.8
+# APP_STL := stlport_shared  --> does not seem to contain C++11 features
+#APP_STL := gnustl_shared
+# Enable c++11 extentions in source code
+#APP_CPPFLAGS += -std=c++11
+
+LOCAL_CFLAGS := -std=gnu++11
+
+
 LOCAL_MODULE := hellocpp_shared
 
 LOCAL_MODULE_FILENAME := libhellocpp
